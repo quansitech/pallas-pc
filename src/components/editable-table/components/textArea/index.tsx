dcbe0fd6744db3c40e5c28762eeabd39d91e8e43
langlixiaoheihong <1591486641@qq.com>
2024-04-09 10:19:52 +0800

    add

--- src/components/editable-table/components/textArea/index.tsx
+++ src/components/editable-table/components/textArea/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef, useCallback} from "react";
-import { Input as AntInput } from "antd";
+import { Input as AntInput, ConfigProvider } from "antd";
 import "./index.css";
 
 const getHeigth = (node: any) => {
@@ -20,20 +20,24 @@ const TextArea:React.FC<any> = (props) => {
 
     const [focus, setFocus] = useState(false);
 
+    const { componentDisabled } = ConfigProvider.useConfig();
+
     const textAreaRef = useCallback((node: any) => {
-        if (node && node.resizableTextArea && node.resizableTextArea.textArea) {
+        if (rest.autoSize && componentDisabled && node && node.resizableTextArea && node.resizableTextArea.textArea) {
             rest.autoSize && (node.resizableTextArea.textArea.style.height = `${getHeigth(node.resizableTextArea.textArea)}px`);
         }
-    }, []);
+    }, [componentDisabled]);
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         props.onChange && props.onChange(e.target.value);
     };
 
+    const val = rest.value || rest.defaultValue || '';
+
     return <div className="component_editable_table_text_area">
         <AntInput.TextArea ref={textAreaRef} className={`component_editable_table_text_area_input ${focus ? 'focus' : ''} ${className }`} onChange={handleChange} onFocus={() => {setFocus(true);}} onBlur={() => {setFocus(false);}}  {...rest} />
         {
-            focus && rest.maxLength ? <div className="component_editable_table_text_area_length">{`${rest?.value?.length ? rest.value.length : 0} / ${rest.maxLength}`}</div> : ''
+            focus && rest.maxLength ? <div className="component_editable_table_text_area_length">{`${val.length} / ${rest.maxLength}`}</div> : ''
         }
     </div>
 }
