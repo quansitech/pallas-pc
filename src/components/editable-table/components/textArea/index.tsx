b52001f2e250ab73c78f79110d08ab6798f917a1
langlixiaoheihong <1591486641@qq.com>
2024-04-09 10:04:36 +0800

    add

--- src/components/editable-table/components/textArea/index.tsx
+++ src/components/editable-table/components/textArea/index.tsx
@@ -1,18 +1,37 @@
-import React, {useState} from "react";
+import React, {useEffect, useState, useRef, useCallback} from "react";
 import { Input as AntInput } from "antd";
 import "./index.css";
+
+const getHeigth = (node: any) => {
+    const style = window.getComputedStyle(node);
+    var boxSizing = style.getPropertyValue('box-sizing') || style.getPropertyValue('-moz-box-sizing') || style.getPropertyValue('-webkit-box-sizing');
+    var paddingSize = parseFloat(style.getPropertyValue('padding-bottom')) + parseFloat(style.getPropertyValue('padding-top'));
+    var borderSize = parseFloat(style.getPropertyValue('border-bottom-width')) + parseFloat(style.getPropertyValue('border-top-width'));
+    let height = node.scrollHeight;
+    if (boxSizing === 'border-box') {
+        return height += borderSize;
+    } else if (boxSizing === 'content-box') {
+        return height -= paddingSize;
+    }
+}
 
 const TextArea:React.FC<any> = (props) => {
     const { onChange, className='',  ...rest } = props;
 
     const [focus, setFocus] = useState(false);
 
+    const textAreaRef = useCallback((node: any) => {
+        if (node && node.resizableTextArea && node.resizableTextArea.textArea) {
+            rest.autoSize && (node.resizableTextArea.textArea.style.height = `${getHeigth(node.resizableTextArea.textArea)}px`);
+        }
+    }, []);
+
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         props.onChange && props.onChange(e.target.value);
-    }
+    };
 
     return <div className="component_editable_table_text_area">
-        <AntInput.TextArea className={`component_editable_table_text_area_input ${focus ? 'focus' : ''} ${className }`} onChange={handleChange} onFocus={() => {setFocus(true);}} onBlur={() => {setFocus(false);}}  {...rest} />
+        <AntInput.TextArea ref={textAreaRef} className={`component_editable_table_text_area_input ${focus ? 'focus' : ''} ${className }`} onChange={handleChange} onFocus={() => {setFocus(true);}} onBlur={() => {setFocus(false);}}  {...rest} />
         {
             focus && rest.maxLength ? <div className="component_editable_table_text_area_length">{`${rest?.value?.length ? rest.value.length : 0} / ${rest.maxLength}`}</div> : ''
         }
